0b6ce8f2075e6b20ad6bd759d3dcc409b259ce2f
the22mastermind <bertrandmasabo22@gmail.com>
2020-03-01 14:39:11 +0200

    refactor(setup API documentation):Move app.js code into index.js [Fixes #171380741]

--- src/index.js
+++ src/index.js
@@ -1,93 +1,25 @@
-const fs = require("fs"),
-    http = require("http"),
-    path = require("path"),
-    methods = require("methods"),
-    express = require("express"),
-    bodyParser = require("body-parser"),
-    session = require("express-session"),
-    cors = require("cors"),
-    passport = require("passport"),
-    errorhandler = require("errorhandler"),
-    mongoose = require("mongoose");
+import express from 'express';
+import { serve, setup } from 'swagger-ui-express';
+import SwaggerSpecs from '../public/api-docs/swagger.json';
 
-const isProduction = process.env.NODE_ENV === "production";
-
-// Create global app object
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(cors());
+const router = express.Router();
 
-// Normal express config defaults
-app.use(require("morgan")("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+router.use('/public/api-docs', serve, setup(SwaggerSpecs));
 
-app.use(require("method-override")());
-app.use(express.static(__dirname + "/public"));
+app.use(router);
 
-app.use(
-    session({
-        secret: "authorshaven",
-        cookie: { maxAge: 60000 },
-        resave: false,
-        saveUninitialized: false
-    })
-);
-
-if (!isProduction) {
-    app.use(errorhandler());
-}
-
-if (isProduction) {
-    mongoose.connect(process.env.MONGODB_URI);
-} else {
-    mongoose.connect("mongodb://localhost/conduit");
-    mongoose.set("debug", true);
-}
-
-require("./models/User");
-
-app.use(require("./routes"));
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    const err = new Error("Not Found");
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (!isProduction) {
-    app.use(function(err, req, res, next) {
-        console.log(err.stack);
-
-        res.status(err.status || 500);
-
-        res.json({
-            errors: {
-                message: err.message,
-                error: err
-            }
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-        errors: {
-            message: err.message,
-            error: {}
-        }
+app.get('/api/v1', (req, res) => {
+    res.status(200).json({
+        status: 200,
+        message: 'Welcome!',
     });
 });
 
-// finally, let's start our server...
-const server = app.listen(process.env.PORT || 3000, function() {
-    console.log("Listening on port " + server.address().port);
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
 });
+
+export default app;
