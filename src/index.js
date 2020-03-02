165cbfb1523ceae200a4e94f4c73b065515b3749
karamuka <shemap300@gmail.com>
2020-03-02 20:36:24 +0200

    chore(dotenv): setup dotenv

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
